02b982a5ae144dbd5434a42bdcc95ccd6c2583d0
brandonT2002 <brandonteja02@gmail.com>
2024-03-11 17:24:25 -0600

    [add] utils

---
+++ src/Classes/Structs/SizeOf.go
@@ -0,0 +1,21 @@
+package structs
+
+func SizeOfBlockFolder() int {
+	return 64
+}
+
+func SizeOfSuperBlock() int {
+	return 68
+}
+
+func SizeOfInode() int {
+	return 88
+}
+
+func SizeOfJournal() int {
+	return 212
+}
+
+func SizeOfBlockPointers() int {
+	return 64
+}
